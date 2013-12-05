23241d935d0897a1c235b4bd95f14eb4ecf44bc0
Pierre Lestringant <pierre.lestringant@gmail.com>
2013-12-05 19:00:59 +0100

    Add AES implementation to the crypto folder

---
+++ algoCrypto/AES.h
@@ -0,0 +1,66 @@
+#ifndef AES_H
+#define AES_H
+
+#include <stdint.h>
+
+#define AES_128_NB_BIT_KEY 			128
+#define AES_128_NB_BYTE_KEY			16
+#define AES_128_NB_WORD_KEY 		4
+#define AES_128_NB_BIT_ROUND_KEY	1408
+#define AES_128_NB_BYTE_ROUND_KEY	176
+#define AES_128_NB_WORD_ROUND_KEY	44
+#define AES_128_NB_ROUND 			10
+
+#define AES_192_NB_BIT_KEY 			192
+#define AES_192_NB_BYTE_KEY 		24
+#define AES_192_NB_WORD_KEY 		6
+#define AES_192_NB_BIT_ROUND_KEY	1664
+#define AES_192_NB_BYTE_ROUND_KEY	208
+#define AES_192_NB_WORD_ROUND_KEY	52
+#define AES_192_NB_ROUND 			12
+
+#define AES_256_NB_BIT_KEY 			256
+#define AES_256_NB_BYTE_KEY 		32
+#define AES_256_NB_WORD_KEY 		8
+#define AES_256_NB_BIT_ROUND_KEY 	1920
+#define AES_256_NB_BYTE_ROUND_KEY 	240
+#define AES_256_NB_WORD_ROUND_KEY 	60
+#define AES_256_NB_ROUND 			14
+
+#define AES_BLOCK_NB_BIT 			128
+#define AES_BLOCK_NB_BYTE 			16
+
+
+/* 
+ * Key schedule - normally the same round key is used for encryption and decryption but here it's a little bit tricky due to our implementation
+ * - key			: 128, 192 or 256 bits
+ * - round_key 		: 44, 52, 60 words
+ */
+
+void aes128_key_expand_encrypt(uint32_t* key, uint32_t* round_key);
+void aes128_key_expand_decrypt(uint32_t* key, uint32_t* round_key);
+
+void aes192_key_expand_encrypt(uint32_t* key, uint32_t* round_key);
+void aes192_key_expand_decrypt(uint32_t* key, uint32_t* round_key);
+
+void aes256_key_expand_encrypt(uint32_t* key, uint32_t* round_key);
+void aes256_key_expand_decrypt(uint32_t* key, uint32_t* round_key);
+
+/* 
+ * Encrypt / decrypt one block of data (128 bits)
+ * - input			: 128 bits (plaintext for encryption and ciphertext for decryption)
+ * - round_key 		: 44, 52, 60 words
+ * - output 		: 128 bits (ciphertext for encryption and plaintext for decryption)
+ */
+
+void aes128_encrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+void aes128_decrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+
+void aes192_encrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+void aes192_decrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+
+void aes256_encrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+void aes256_decrypt(uint32_t* input, uint32_t* round_key, uint32_t* output);
+
+
+#endif
---
+++ algoCrypto/example/aes_test.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../AES.h"
+#include "../../misc/printBuffer.h"
+
+/*Those test vectors are taken from: http://www.inconteam.com/software-development/41-encryption/55-aes-test-vectors#aes-ecb */
+
+int main(){
+	unsigned char plaintext[AES_BLOCK_NB_BYTE] = {0xe2, 0xbe, 0xc1, 0x6b, 0x96, 0x9f, 0x40, 0x2e, 0x11, 0x7e, 0x3d, 0xe9, 0x2a, 0x17, 0x93, 0x73};
+	
+	unsigned char key_128[AES_128_NB_BYTE_KEY] = {0x16, 0x15, 0x7e, 0x2b, 0xa6, 0xd2, 0xae, 0x28, 0x88, 0x15, 0xf7, 0xab, 0x3c, 0x4f, 0xcf, 0x09};
+	unsigned char key_192[AES_192_NB_BYTE_KEY] = {0xf7, 0xb0, 0x73, 0x8e, 0x52, 0x64, 0x0e, 0xda, 0x2b, 0xf3, 0x10, 0xc8, 0xe5, 0x79, 0x90, 0x80, 0xd2, 0xea, 0xf8, 0x62, 0x7b, 0x6b, 0x2c, 0x52};
+	unsigned char key_256[AES_256_NB_BYTE_KEY] = {0x10, 0xeb, 0x3d, 0x60, 0xbe, 0x71, 0xca, 0x15, 0xf0, 0xae, 0x73, 0x2b, 0x81, 0x77, 0x7d, 0x85, 0x07, 0x2c, 0x35, 0x1f, 0xd7, 0x08, 0x61, 0x3b, 0xa3, 0x10, 0x98, 0x2d, 0xf4, 0xdf, 0x14, 0x09};
+
+	unsigned char round_key_128[AES_128_NB_BYTE_ROUND_KEY];
+	unsigned char round_key_192[AES_192_NB_BYTE_ROUND_KEY];
+	unsigned char round_key_256[AES_256_NB_BYTE_ROUND_KEY];
+
+	unsigned char ciphertext[AES_BLOCK_NB_BYTE];
+	unsigned char deciphertext[AES_BLOCK_NB_BYTE];
+
+	
+	printf("Plaintext:      ");
+	printBuffer_raw(stdout, (char*)plaintext, AES_BLOCK_NB_BYTE);
+
+	/* 128 bits AES */
+	printf("\nKey 128:        ");
+	printBuffer_raw(stdout, (char*)key_128, AES_128_NB_BYTE_KEY);
+
+	aes128_key_expand_encrypt((uint32_t*)key_128, (uint32_t*)round_key_128);
+	aes128_encrypt((uint32_t*)plaintext, (uint32_t*)round_key_128, (uint32_t*)ciphertext);
+
+	printf("\nCiphertext 128: ");
+	printBuffer_raw(stdout, (char*)ciphertext, AES_BLOCK_NB_BYTE);
+
+ 	aes128_key_expand_decrypt((uint32_t*)key_128, (uint32_t*)round_key_128);
+	aes128_decrypt((uint32_t*)ciphertext, (uint32_t*)round_key_128, (uint32_t*)deciphertext);
+
+	if (memcmp(deciphertext, plaintext, AES_BLOCK_NB_BYTE) == 0){
+		printf("\nRecovery 128:   OK");
+	}
+	else{
+		printf("\nRecovery 128:   FAIL");
+	}
+
+	/* 192 bits AES */
+	printf("\nKey 192:        ");
+	printBuffer_raw(stdout, (char*)key_192, AES_192_NB_BYTE_KEY);
+
+	aes192_key_expand_encrypt((uint32_t*)key_192, (uint32_t*)round_key_192);
+	aes192_encrypt((uint32_t*)plaintext, (uint32_t*)round_key_192, (uint32_t*)ciphertext);
+
+	printf("\nCiphertext 192: ");
+	printBuffer_raw(stdout, (char*)ciphertext, AES_BLOCK_NB_BYTE);
+
+ 	aes192_key_expand_decrypt((uint32_t*)key_192, (uint32_t*)round_key_192);
+	aes192_decrypt((uint32_t*)ciphertext, (uint32_t*)round_key_192, (uint32_t*)deciphertext);
+
+	if (memcmp(deciphertext, plaintext, AES_BLOCK_NB_BYTE) == 0){
+		printf("\nRecovery 192:   OK");
+	}
+	else{
+		printf("\nRecovery 192:   FAIL");
+	}
+
+	/* 256 bits AES */
+	printf("\nKey 256:        ");
+	printBuffer_raw(stdout, (char*)key_256, AES_256_NB_BYTE_KEY);
+
+	aes256_key_expand_encrypt((uint32_t*)key_256, (uint32_t*)round_key_256);
+	aes256_encrypt((uint32_t*)plaintext, (uint32_t*)round_key_256, (uint32_t*)ciphertext);
+
+	printf("\nCiphertext 256: ");
+	printBuffer_raw(stdout, (char*)ciphertext, AES_BLOCK_NB_BYTE);
+
+	aes256_key_expand_decrypt((uint32_t*)key_256, (uint32_t*)round_key_256);
+	aes256_decrypt((uint32_t*)ciphertext, (uint32_t*)round_key_256, (uint32_t*)deciphertext);
+
+	if (memcmp(deciphertext, plaintext, AES_BLOCK_NB_BYTE) == 0){
+		printf("\nRecovery 256:   OK\n");
+	}
+	else{
+		printf("\nRecovery 256:   FAIL\n");
+	}
+
+	return 0;
+}
