045646b45755fcb78c48161631740377633aa2c1
Pierre Lestringant <pierre.lestringant@gmail.com>
2014-04-15 09:34:40 +0200

    Add a pageTable structure to the misc folder. It might be usefull for fast constant detection and also for memory renaming as part of the IR translation process (not sure)

---
+++ misc/pageTable.c
@@ -0,0 +1,343 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "pageTable.h"
+
+struct pageTable* pageTable_create(size_t data_size){
+	struct pageTable* pt;
+
+	pt = (struct pageTable*)malloc(sizeof(struct pageTable));
+	if (pt != NULL){
+		if (pageTable_init(pt, data_size)){
+			printf("ERROR: in %s, unable to init pageTable\n", __func__);
+			free(pt);
+			pt = NULL;
+		}
+	}
+	else{
+		printf("ERROR: in %s, unable to allocate memory\n", __func__);
+	}
+
+	return pt;
+}
+
+int32_t pageTable_init(struct pageTable* pt, size_t data_size){
+	uint16_t i;
+
+	if (data_size < 1){
+		printf("ERROR: in %s, incorrect data size\n", __func__);
+		return -1;
+	}
+
+	pt->data_size = data_size;
+
+	#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+	pt->memory_consumption = sizeof(struct pageTable);
+	#endif
+
+	for (i = 0; i < 256; i++){
+		pt->main_pages[i] = NULL;
+	}
+
+	return 0;
+}
+
+#define PAGETABLE_GET_INDEX1(index) 				((index) >> 24)
+#define PAGETABLE_GET_INDEX2(index) 				(((index) >> 16) & 0x000000ff)
+#define PAGETABLE_GET_INDEX3(index) 				(((index) >> 8) & 0x000000ff)
+#define PAGETABLE_GET_INDEX4(index) 				((index) & 0x000000ff)
+
+#define PAGETABLE_SET_VALUE(index, set_vector) 		((set_vector)[(index) >> 5] |=  0x00000001 << ((index) & 0x0000001f))
+#define PAGETABLE_CLEAN_VALUE(index, set_vector) 	((set_vector)[(index) >> 5] &=  ~(0x00000001 << ((index) & 0x0000001f)))
+#define PAGETABLE_IS_VALUE_SET(index, set_vector) 	(((set_vector)[(index) >> 5] >> ((index) & 0x0000001f)) & 0x00000001)
+
+int32_t pageTable_add_element(struct pageTable* pt, uint32_t index, void* data){
+	struct pageIndex* 	sub_page1;
+	struct pageIndex* 	sub_page2;
+	struct pageData* 	sub_page3;
+
+	uint8_t 			index1 = PAGETABLE_GET_INDEX1(index);
+	uint8_t 			index2 = PAGETABLE_GET_INDEX2(index);
+	uint8_t 			index3 = PAGETABLE_GET_INDEX3(index);
+	uint8_t 			index4 = PAGETABLE_GET_INDEX4(index);
+
+	if (pt->main_pages[index1] == NULL){
+		pt->main_pages[index1] = (struct pageIndex*)malloc(sizeof(struct pageIndex));
+		if (pt->main_pages[index1] == NULL){
+			printf("ERROR: in %s, unable to allocate memory\n", __func__);
+			return -1;
+		}
+		else{
+			sub_page1 = pt->main_pages[index1];
+			sub_page1->nb_element = 0;
+			memset(sub_page1->pages, 0, 256*sizeof(void*));
+
+			#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+			pt->memory_consumption += sizeof(struct pageIndex);
+			#endif
+		}	
+	}
+	else{
+		sub_page1 = pt->main_pages[index1];
+	}
+
+	if (sub_page1->pages[index2] == NULL){
+		sub_page1->pages[index2] = malloc(sizeof(struct pageIndex));
+		if (sub_page1->pages[index2] == NULL){
+			printf("ERROR: in %s, unable to allocate memory\n", __func__);
+			return -1;
+		}
+		else{
+			sub_page1->nb_element ++;
+			sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+			sub_page2->nb_element = 0;
+			memset(sub_page2->pages, 0, 256*sizeof(void*));
+
+			#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+			pt->memory_consumption += sizeof(struct pageIndex);
+			#endif
+		}	
+	}
+	else{
+		sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+	}
+
+	if (sub_page2->pages[index3] == NULL){
+		sub_page2->pages[index3] = (struct pageData*)malloc(sizeof(struct pageData) + 256 * (pt->data_size - 1));
+		if (sub_page2->pages[index3] == NULL){
+			printf("ERROR: in %s, unable to allocate memory\n", __func__);
+			return -1;
+		}
+		else{
+			sub_page2->nb_element ++;
+			sub_page3 = (struct pageData*)sub_page2->pages[index3];
+			sub_page3->nb_element = 0;
+			memset(sub_page3->set_vector, 0, 8*sizeof(uint32_t));
+
+			#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+			pt->memory_consumption += sizeof(struct pageData) + 256 * (pt->data_size - 1);
+			#endif
+		}	
+	}
+	else{
+		sub_page3 = (struct pageData*)sub_page2->pages[index3];
+	}
+
+	memcpy(sub_page3->data + pt->data_size * index4, data, pt->data_size);
+	PAGETABLE_SET_VALUE(index4, sub_page3->set_vector);
+	sub_page3->nb_element ++;
+
+	return 0;
+}
+
+void* pageTable_get_element(struct pageTable* pt, uint32_t index){
+	struct pageIndex* 	sub_page1;
+	struct pageIndex* 	sub_page2;
+	struct pageData* 	sub_page3;
+
+	uint8_t 			index1 = PAGETABLE_GET_INDEX1(index);
+	uint8_t 			index2 = PAGETABLE_GET_INDEX2(index);
+	uint8_t 			index3 = PAGETABLE_GET_INDEX3(index);
+	uint8_t 			index4 = PAGETABLE_GET_INDEX4(index);
+
+	sub_page1 = pt->main_pages[index1];
+	if (sub_page1 == NULL){
+		return NULL;
+	}
+
+	sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+	if (sub_page2 == NULL){
+		return NULL;
+	}
+
+	sub_page3 = (struct pageData*)sub_page2->pages[index3];
+	if (sub_page3 == NULL){
+		return NULL;
+	}
+
+	if (PAGETABLE_IS_VALUE_SET(index4, sub_page3->set_vector)){
+		return sub_page3->data + pt->data_size * index4;
+	}
+	else{
+		return NULL;
+	}
+}
+
+void* pageTable_get_next(struct pageTable* pt, uint32_t* index){
+	struct pageIndex* 	sub_page1;
+	struct pageIndex* 	sub_page2;
+	struct pageData* 	sub_page3;
+
+	uint8_t 			index1;
+	uint8_t 			index2;
+	uint8_t 			index3;
+	uint8_t 			index4;
+
+	if (*index == 0xffffffff){
+		return NULL;
+	}
+
+	*index = *index + 1;
+
+	index1 = PAGETABLE_GET_INDEX1(*index);
+	index2 = PAGETABLE_GET_INDEX2(*index);
+	index3 = PAGETABLE_GET_INDEX3(*index);
+	index4 = PAGETABLE_GET_INDEX4(*index);
+
+	sub_page1 = pt->main_pages[index1];
+	if (sub_page1 == NULL){
+		search_index1:
+
+		index2 = 0;
+		index3 = 0;
+		index4 = 0;
+		
+		while(sub_page1 == NULL && index1 != 255){
+			index1 ++;
+			sub_page1 = pt->main_pages[index1];
+		}
+		if (sub_page1 == NULL){
+			return NULL;
+		}
+	}
+
+	sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+	if (sub_page2 == NULL){
+		search_index2:
+
+		index3 = 0;
+		index4 = 0;
+		
+		while(sub_page2 == NULL && index2 != 255){
+			index2 ++;
+			sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+		}
+		if (sub_page2 == NULL){
+			sub_page1 = NULL;
+			goto search_index1;
+		}
+	}
+
+	sub_page3 = (struct pageData*)sub_page2->pages[index3];
+	if (sub_page3 == NULL){
+		search_index3:
+
+		index4 = 0;
+
+		while(sub_page3 == NULL && index3 != 255){
+			index3 ++;
+			sub_page3 = (struct pageData*)sub_page2->pages[index3];
+		}
+		if (sub_page3 == NULL){
+			sub_page2 = 0;
+			goto search_index2;
+		}
+	}
+
+	while(!PAGETABLE_IS_VALUE_SET(index4, sub_page3->set_vector) && index4 != 255){
+		index4 ++;
+	}
+	if (!PAGETABLE_IS_VALUE_SET(index4, sub_page3->set_vector)){
+		sub_page3 = NULL;
+		goto search_index3;
+	}
+	
+	*index = (index1 << 24) | (index2 << 16) | (index3 << 8) | index4;
+
+	return sub_page3->data + pt->data_size * index4;
+}
+
+void pageTable_remove_element(struct pageTable* pt, uint32_t index){
+	struct pageIndex* 	sub_page1;
+	struct pageIndex* 	sub_page2;
+	struct pageData* 	sub_page3;
+
+	uint8_t 			index1 = PAGETABLE_GET_INDEX1(index);
+	uint8_t 			index2 = PAGETABLE_GET_INDEX2(index);
+	uint8_t 			index3 = PAGETABLE_GET_INDEX3(index);
+	uint8_t 			index4 = PAGETABLE_GET_INDEX4(index);
+
+	sub_page1 = pt->main_pages[index1];
+	if (sub_page1 != NULL){
+
+		sub_page2 = (struct pageIndex*)sub_page1->pages[index2];
+		if (sub_page2 != NULL){
+			
+			sub_page3 = (struct pageData*)sub_page2->pages[index3];
+			if (sub_page3 != NULL){
+				
+				if (PAGETABLE_IS_VALUE_SET(index4, sub_page3->set_vector)){
+					PAGETABLE_CLEAN_VALUE(index4, sub_page3->set_vector);
+					sub_page3->nb_element --;
+
+					if (sub_page3->nb_element == 0){
+						free(sub_page2->pages[index3]);
+						sub_page2->pages[index3] = NULL;
+						sub_page2->nb_element --;
+
+						#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+						pt->memory_consumption -= sizeof(struct pageData) + 256 * (pt->data_size - 1);
+						#endif
+
+						if (sub_page2->nb_element == 0){
+							free(sub_page1->pages[index2]);
+							sub_page1->pages[index2] = NULL;
+							sub_page1->nb_element --;
+
+							#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+							pt->memory_consumption -= sizeof(struct pageIndex);
+							#endif
+
+							if (sub_page1->nb_element == 0){
+								free(pt->main_pages[index1]);
+								pt->main_pages[index1] = NULL;
+
+								#ifdef PAGETABLE_TRACK_MEMORY_CONSUMPTION
+								pt->memory_consumption -= sizeof(struct pageIndex);
+								#endif
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void pageTable_clean(struct pageTable* pt){
+	struct pageIndex* 	sub_page1;
+	struct pageIndex* 	sub_page2;
+	uint16_t 			i1;
+	uint16_t 			i2;
+	uint16_t 			i3;
+
+	for (i1 = 0; i1 < 256; i1++){
+		if (pt->main_pages[i1] != NULL){
+			sub_page1 = (struct pageIndex*)pt->main_pages[i1];
+
+			for (i2 = 0; i2 < 256; i2++){
+				if (sub_page1->pages[i2] != NULL){
+					sub_page2 = (struct pageIndex*)sub_page1->pages[i2];
+
+					for (i3 = 0; i3 < 256; i3++){
+						if (sub_page2->pages[i3] != NULL){
+							free(sub_page2->pages[i3]);
+						}
+					}
+
+					free(sub_page1->pages[i2]);
+				}
+			}
+
+			free(pt->main_pages[i1]);
+		}
+	}
+}
+
+void pageTable_delete(struct pageTable* pt){
+	if (pt != NULL){
+		pageTable_clean(pt);
+		free(pt);
+	}
+}
---
+++ misc/test/test_pageTable.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../pageTable.h"
+
+#define NB_ELEMENT 		12
+#define INDEX_TO_REMOVE 5
+
+int main(){
+	struct pageTable* 	pt;
+	uint32_t 			i;
+	uint32_t 			elements_address[NB_ELEMENT] = {0x00000012, 0x00000056, 0xffffffff, 0x12345678, 0x74561232, 0x45567898, 0x00000000, 0x00004565, 0x12650000, 0x012305604, 0x00369874, 0xab4562f1};
+	uint32_t 			elements_value[NB_ELEMENT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	uint32_t 			read_address;
+	uint32_t* 			read_value;
+
+	pt = pageTable_create(sizeof(uint32_t));
+	if (pt == NULL){
+		printf("ERROR: in %s, unable to create pageTable\n", __func__);
+	}
+
+	printf("Trying to add %u element(s) to the page table\n", NB_ELEMENT);
+
+	for (i = 0; i < NB_ELEMENT; i++){
+		if (pageTable_add_element(pt, elements_address[i], elements_value + i)){
+			printf("ERROR: in %s, unable to add element in pageTable\n", __func__);
+			break;
+		}
+	}
+
+	read_address = 0;
+	read_value = (uint32_t*)pageTable_get_next(pt, &read_address);
+	while(read_value != NULL){
+		printf("Read value: %u \t@ 0x%08x\n", *read_value, read_address);
+		read_value = (uint32_t*)pageTable_get_next(pt, &read_address);
+	}
+	
+	printf("Trying to remove element %u\n", elements_value[INDEX_TO_REMOVE]);
+
+	pageTable_remove_element(pt, elements_address[INDEX_TO_REMOVE]);
+
+	read_address = 0;
+	read_value = (uint32_t*)pageTable_get_next(pt, &read_address);
+	while(read_value != NULL){
+		printf("Read value: %u \t@ 0x%08x\n", *read_value, read_address);
+		read_value = (uint32_t*)pageTable_get_next(pt, &read_address);
+	}
+
+	printf("Memory consumption: %u byte(s)\n", pageTable_get_memory_consumption(pt));
+
+	pageTable_delete(pt);
+
+	return 0;
+}
