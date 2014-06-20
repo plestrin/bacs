dfb6207303dd79c02eb3f84fd9e92be4eed307bd
Pierre Lestringant <pierre.lestringant@gmail.com>
2014-06-20 09:54:30 +0200

    Add Crypto++ MD5 example

---
+++ algoCrypto/cryptopp/src/cryptopp_md5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+
+#include <cryptopp/md5.h>
+
+void print_raw_buffer(byte* buffer, int buffer_length);
+
+int main(int argc, char* argv[]) {
+	char 				message[] = "12345678901234567890123456789012345678901234567890123456789012345678901234567890";
+	byte 				hash[CryptoPP::Weak::MD5::DIGESTSIZE];
+	CryptoPP::Weak::MD5 md5;
+
+	md5.CalculateDigest(hash, (unsigned char*)message, strlen(message));
+
+	std::cout << "Plaintext: " << message << std::endl;
+	std::cout << "MD5 hash:  ";
+	print_raw_buffer(hash, sizeof(hash));
+	std::cout << std::endl;
+
+	return 0;
+}
+
+void print_raw_buffer(byte* buffer, int buffer_length){
+	for (int i = 0; i < buffer_length; i++){
+		if (buffer[i] & 0xf0){
+			std::cout << std::hex << (buffer[i] & 0xff);
+		}
+		else{
+			std::cout << "0" << std::hex << (buffer[i] & 0xff);
+		}
+	}
+}
