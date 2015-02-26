ae617bd7b4fc44c4003a22e156207b9795e9bf23
Pierre Lestringant <pierre.lestringant@gmail.com>
2015-02-26 18:33:38 +0100

    Add libTomCrypt SHA256 and SHA512 implementation, not very usefull but I needed them for another test. Anyway here they are

---
+++ algoCrypto/tomCrypt/src/tom_sha256.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <tomcrypt.h>
+
+#include "printBuffer.h"
+
+int main(){
+	char 		message[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+	uint32_t 	hash[8];
+	hash_state 	md;
+	
+	sha256_init(&md);
+	sha256_process(&md, (unsigned char*)message, strlen(message));
+	sha256_done(&md, (unsigned char*)hash);
+
+	printf("Plaintext:   \"%s\"\n", message);
+	printf("SHA256 hash: ");
+	printBuffer_raw(stdout, (char*)hash, sizeof(hash));
+	printf("\n");
+
+	return 0;
+}
---
+++ algoCrypto/tomCrypt/src/tom_sha512.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <tomcrypt.h>
+
+#include "printBuffer.h"
+
+int main(){
+	char 		message[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstuabcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+	uint32_t 	hash[16];
+	hash_state 	md;
+	
+	sha512_init(&md);
+	sha512_process(&md, (unsigned char*)message, strlen(message));
+	sha512_done(&md, (unsigned char*)hash);
+
+	printf("Plaintext:   \"%s\"\n", message);
+	printf("SHA512 hash: ");
+	printBuffer_raw(stdout, (char*)hash, sizeof(hash));
+	printf("\n");
+
+	return 0;
+}
