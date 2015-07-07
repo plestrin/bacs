205f39b873eb893bf3bcc57dba5719d1bbc8669e
Pierre Lestringant <pierre.lestringant@gmail.com>
2015-07-07 19:04:15 +0200

    [Minor] add new functionnality for the file mapping

--- misc/mapFile.c
+++ misc/mapFile.c
@@ -8,6 +8,7 @@
 #include <sys/mman.h>
 
 #include "mapFile.h"
+#include "base.h"
 
 void* mapFile_map(const char* file_name, size_t* size){
 	int 				file;
@@ -16,13 +17,12 @@ void* mapFile_map(const char* file_name, size_t* size){
 
 	file = open(file_name, O_RDONLY);
 	if (file == -1){
-		printf("ERROR: in %s, unable to open file %s read only\n", __func__, file_name);
-		perror(NULL);
+		log_err_m("unable to open file %s read only", file_name);
 		return NULL;
 	}
 
 	if (fstat(file, &sb) < 0){
-		printf("ERROR: in %s, unable to read file size\n", __func__);
+		log_err("unable to read file size");
 		close(file);
 		return NULL;
 	}
@@ -31,8 +31,28 @@ void* mapFile_map(const char* file_name, size_t* size){
 	buffer = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, file, 0);
 	close(file);
 	if (buffer == NULL){
-		printf("ERROR: in %s, unable to map file\n", __func__);
+		log_err("unable to map file");
 	}
 
 	return buffer;
+}
+
+void* mapFile_part(int file, off_t offset, size_t size, struct mappingDesc* desc){
+	long 	page_size;
+	size_t 	align;
+
+	if ((page_size = sysconf(_SC_PAGE_SIZE)) == -1){
+		log_err("unable get PAGE_SIZE value");
+		return NULL;
+	}
+
+	align = offset % page_size;
+	desc->size = size + align;
+	desc->buffer = mmap(NULL, size + align, PROT_READ, MAP_PRIVATE, file, offset - align);
+	if (desc->buffer == NULL){
+		log_err("unable to map file");
+		return NULL;
+	}
+
+	return (char*)desc->buffer + align;
 }
--- misc/mapFile.h
+++ misc/mapFile.h
@@ -2,7 +2,17 @@
 #define MAPFILE_H
 
 #include <stdint.h>
+#include <sys/mman.h>
+
+struct mappingDesc{
+	void*	buffer;
+	size_t 	size;
+};
+
+#define mappingDesc_free_mapping(desc) munmap((desc).buffer, (desc).size)
 
 void* mapFile_map(const char* file_name, size_t* size);
+
+void* mapFile_part(int file, off_t offset, size_t size, struct mappingDesc* desc);
 
 #endif
