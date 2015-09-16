b23d6c6ed115ff7bbc5a7b50e446032e9df37ad8
Pierre Lestringant <pierre.lestringant@gmail.com>
2015-09-16 11:59:33 +0200

    Add in the crypto folder lib Nettle *_AES implem

---
+++ algoCrypto/nettle/src/nettle_aes.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <nettle/aes.h>
+
+#include "printBuffer.h"
+
+int main(){
+	uint8_t 		key128[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	uint8_t 		key192[24] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17};
+	uint8_t 		key256[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+
+	unsigned char 	ct[16];
+	unsigned char 	vt[16];
+	unsigned char 	pt[16] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+	struct aes_ctx 	ctx;
+	
+
+	/* AES 128 */
+	aes_set_encrypt_key(&ctx, sizeof(key128), key128);
+	aes_encrypt(&ctx, sizeof(pt), ct, pt);
+	aes_set_decrypt_key(&ctx, sizeof(key128), key128);
+	aes_decrypt(&ctx, sizeof(ct), vt, ct);
+
+	printf("Plaintext:      ");
+	printBuffer_raw(stdout, (char*)pt, 16);
+	printf("\nKey 128:        ");
+	printBuffer_raw(stdout, (char*)key128, 16);
+	printf("\nCiphertext 128: ");
+	printBuffer_raw(stdout, (char*)ct, 16);
+
+	if (!memcmp(vt, pt, 16)){
+		printf("\nRecovery 128:   OK\n");
+	}
+	else{
+		printf("\nRecovery 128:   FAIL\n");
+	}
+
+	/* AES 192 */
+	aes_set_encrypt_key(&ctx, sizeof(key192), key192);
+	aes_encrypt(&ctx, sizeof(pt), ct, pt);
+	aes_set_decrypt_key(&ctx, sizeof(key192), key192);
+	aes_decrypt(&ctx, sizeof(ct), vt, ct);
+
+	printf("Key 192:        ");
+	printBuffer_raw(stdout, (char*)key192, 24);
+	printf("\nCiphertext 192: ");
+	printBuffer_raw(stdout, (char*)ct, 16);
+
+	if (!memcmp(vt, pt, 16)){
+		printf("\nRecovery 192:   OK\n");
+	}
+	else{
+		printf("\nRecovery 192:   FAIL\n");
+	}
+
+
+	/* AES 256 */
+	aes_set_encrypt_key(&ctx, sizeof(key256), key256);
+	aes_encrypt(&ctx, sizeof(pt), ct, pt);
+	aes_set_decrypt_key(&ctx, sizeof(key256), key256);
+	aes_decrypt(&ctx, sizeof(ct), vt, ct);
+
+	printf("Key 256:        ");
+	printBuffer_raw(stdout, (char*)key256, 32);
+	printf("\nCiphertext 256: ");
+	printBuffer_raw(stdout, (char*)ct, 16);
+
+	if (!memcmp(vt, pt, 16)){
+		printf("\nRecovery 256:   OK\n");
+	}
+	else{
+		printf("\nRecovery 256:   FAIL\n");
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ algoCrypto/nettle/src/nettle_cbc_aes.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <nettle/aes.h>
+#include <nettle/cbc.h>
+
+#include "printBuffer.h"
+
+#define CBC_SET_ENC_KEY(ctx_, key) aes_set_encrypt_key(&((ctx_)->ctx), 16, key)
+#define CBC_SET_DEC_KEY(ctx_, key) aes_set_decrypt_key(&((ctx_)->ctx), 16, key)
+
+int main(){
+	char 	plaintext[] = "Hi I am an AES CBC test vector distributed on 4 128-bit blocks!";
+	uint8_t key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	uint8_t iv[16] = {0x01, 0xff, 0x83, 0xf2, 0xf9, 0x98, 0xba, 0xa4, 0xda, 0xdc, 0xaa, 0xcc, 0x8e, 0x17, 0xa4, 0x1b};
+	uint8_t ciphertext[sizeof(plaintext)];
+	uint8_t deciphertext[sizeof(plaintext)];
+	struct CBC_CTX(struct aes_ctx, AES_BLOCK_SIZE) ctx;
+	
+	printf("Plaintext:      \"%s\"\n", plaintext);
+	printf("IV:             ");
+	printBuffer_raw(stdout, (char*)iv, sizeof(iv));
+	printf("\nKey 128:        ");
+	printBuffer_raw(stdout, (char*)key, sizeof(key));
+
+	CBC_SET_IV(&ctx, iv);
+	CBC_SET_ENC_KEY(&ctx, key);
+	CBC_ENCRYPT(&ctx, aes_encrypt, sizeof(plaintext), ciphertext, (uint8_t*)plaintext);
+
+	CBC_SET_IV(&ctx, iv);
+	CBC_SET_DEC_KEY(&ctx, key);
+	CBC_DECRYPT(&ctx, aes_decrypt, sizeof(ciphertext), deciphertext, ciphertext);
+
+	printf("\nCiphertext CBC: ");
+	printBuffer_raw(stdout, (char*)ciphertext, sizeof(plaintext));
+
+	if (memcmp(deciphertext, plaintext, sizeof(plaintext)) == 0){
+		printf("\nRecovery:       OK\n");
+	}
+	else{
+		printf("\nRecovery:       FAIL\n");
+	}
+	return EXIT_SUCCESS;
+}
