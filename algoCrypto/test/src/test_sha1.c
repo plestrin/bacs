fcf2958a860154321bf8ba58da62f814c6df15e6
Pierre Lestringant <pierre.lestringant@gmail.com>
2016-07-24 16:59:04 +0200

    [Update] SIG_sha1

--- algoCrypto/test/src/test_sha1.c
+++ algoCrypto/test/src/test_sha1.c
@@ -5,13 +5,13 @@
 #include "SHA1.h"
 #include "printBuffer.h"
 
-#ifdef WIN32
+#ifdef _WIN32
 #include "windowsComp.h"
 #endif
 
 /* This test vector is taken from http://www.di-mgt.com.au/sha_testvectors.html */
 
-int main(){
+int main(void){
 	char 		message[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
 	char* 		padded_message;
 	uint32_t 	message_size;
@@ -35,5 +35,5 @@ int main(){
 		printf("ERROR: in %s, unable to allocate memory\n", __func__);
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
