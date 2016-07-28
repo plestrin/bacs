c7504cfa81ba00b5407d82914eb29a184018d07c
Pierre Lestringant <pierre.lestringant@gmail.com>
2016-07-28 15:24:58 +0200

    [Add] example/ocb

---
+++ example/ocb/src/ocb.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <tomcrypt.h>
+
+#include "printBuffer.h"
+
+int main(void){
+	char 			plaintext[] = "Hi I am an AES OCB test vector! Bye";
+	unsigned char 	key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	unsigned char 	iv[16] = {0x01, 0xff, 0x83, 0xf2, 0xf9, 0x98, 0xba, 0xa4, 0xda, 0xdc, 0xaa, 0xcc, 0x8e, 0x17, 0xa4, 0x1b};
+	unsigned char 	ciphertext[sizeof(plaintext)];
+	unsigned char 	deciphertext[sizeof(plaintext)];
+	int 			err;
+	unsigned char 	tag[16];
+	unsigned long 	tag_len;
+	int 			stat;
+
+	if (register_cipher(&aes_desc) == -1) {
+		printf("Error: in %s, unable to register cipher\n", __func__);
+		return EXIT_FAILURE;
+	}
+	
+	printf("Plaintext:      \"%s\"\n", plaintext);
+	printf("IV:             ");
+	printBuffer_raw(stdout, (char*)iv, sizeof(iv));
+	printf("\nKey 128:        ");
+	printBuffer_raw(stdout, (char*)key, sizeof(key));
+
+	if ((err = ocb_encrypt_authenticate_memory(find_cipher("aes"), key, sizeof(key), iv, (unsigned char*)plaintext, sizeof(plaintext), ciphertext, tag, &tag_len)) != CRYPT_OK){
+		printf("ERROR: in %s, %s\n", __func__, error_to_string(err));
+		return EXIT_FAILURE;
+	}
+
+	if ((err = ocb_decrypt_verify_memory(find_cipher("aes"), key, sizeof(key), iv, ciphertext, sizeof(ciphertext), deciphertext, tag, tag_len, &stat)) != CRYPT_OK){
+		printf("ERROR: in %s, %s\n", __func__, error_to_string(err));
+		return EXIT_FAILURE;
+	}
+
+	printf("\nCiphertext CBC: ");
+	printBuffer_raw(stdout, (char*)ciphertext, sizeof(plaintext));
+
+	if (memcmp(deciphertext, plaintext, sizeof(plaintext)) == 0 && stat){
+		printf("\nRecovery:       OK\n");
+	}
+	else{
+		printf("\nRecovery:       FAIL\n");
+	}
+
+	return EXIT_SUCCESS;
+}
