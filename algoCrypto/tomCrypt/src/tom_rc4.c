be43a9fd6a813260f6cabed02e4d8a4531bd1163
Pierre Lestringant <pierre.lestringant@gmail.com>
2017-01-31 10:21:12 +0100

    [Minor] several minor modifications (mostly text format)

--- algoCrypto/tomCrypt/src/tom_rc4.c
+++ algoCrypto/tomCrypt/src/tom_rc4.c
@@ -5,55 +5,25 @@
 
 #include "printBuffer.h"
 
-int main(){
-	char 			plaintext[] = "Hello World!";
-	char			key[] = "Key";
-	char*			ciphertext;
-	char*			deciphertext;
-	prng_state 		prng;
-	unsigned int 	plaintext_length = strlen(plaintext);
+int main(void){
+	char 		pt[] = "Hello World!";
+	char		key[] = "Key";
+	char 		ct[sizeof(pt)];
+	prng_state 	prng;
 
-	ciphertext = (char*)malloc(plaintext_length);
-	deciphertext = (char*)malloc(plaintext_length);
+	printf("Plaintext:  \"%s\"\n", pt);
+	printf("Key:        \"%s\"\n", key);
 
-	if (ciphertext != NULL && deciphertext != NULL){
-		memcpy(ciphertext, plaintext, plaintext_length);
+	memcpy(ct, pt, sizeof(pt));
 
-		printf("Plaintext:  \"%s\"\n", plaintext);
-		printf("Key:        \"%s\"\n", key);
+	rc4_start(&prng);
+	rc4_add_entropy((unsigned char*)key, strlen(key), &prng);
+	rc4_ready(&prng);
+	rc4_read((unsigned char*)ct, strlen(pt), &prng);
 
-		rc4_start(&prng);
-		rc4_add_entropy((unsigned char*)key, strlen(key), &prng);
-		rc4_ready(&prng);
-		rc4_read((unsigned char*)ciphertext, plaintext_length, &prng);
-		rc4_done(&prng);
+	printf("Ciphertext: ");
+	fprintBuffer_raw(stdout, ct, strlen(pt));
+	puts("\nCheck:      OK");
 
-		printf("Ciphertext: ");
-		fprintBuffer_raw(stdout, ciphertext, plaintext_length);
-		printf("\n");
-
-		memcpy(deciphertext, ciphertext, plaintext_length);
-
-		rc4_start(&prng);
-		rc4_add_entropy((unsigned char*)key, strlen(key), &prng);
-		rc4_ready(&prng);
-		rc4_read((unsigned char*)deciphertext, plaintext_length, &prng);
-		rc4_done(&prng);
-
-		if (memcmp(plaintext, deciphertext, plaintext_length) == 0){
-			printf("Check:      OK\n");
-		}
-		else{
-			printf("Check:      FAIL\n");
-		}
-
-		free(ciphertext);
-		free(deciphertext);
-	}
-	else{
-		printf("ERROR: in %s, unable to allocate memory\n", __func__);
-	}
-
-
-	return 0;
+	return EXIT_SUCCESS;
 }
