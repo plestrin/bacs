a714f5984c53aa63acd23701ac0eeb63ee10d9aa
Pierre Lestringant <pierre.lestringant@gmail.com>
2017-02-01 09:06:47 +0100

    Move the synthetic samples in a dedicated folder

--- syntheticSample/check.py
+++ syntheticSample/check.py
@@ -7,73 +7,73 @@ import subprocess
 import difflib
 
 ckeck_list = [
-	# TEA a XTEA (tomCrypt is not included since it only implements XTEA)
+	# TEA a XTEA (tomcrypt is not included since it only implements XTEA)
 	[
 		r'test/build/test_tea_[gc]o[0-9sz]',
 		r'botan/build/bot_tea',
-		r'cryptopp/build/cryptopp_tea'
+		r'cryptopp/build/pp_tea'
 	],
 
 	# TEA ECB
 	[
 		r'test/build/test_ecb_tea_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_ecb_tea',
+		r'tomcrypt/build/tom_ecb_tea',
 		r'botan/build/bot_ecb_tea'
 	],
 
 	# TEA CBC
 	[
 		r'test/build/test_cbc_tea_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_cbc_tea',
-		r'cryptopp/build/cryptopp_cbc_tea',
+		r'tomcrypt/build/tom_cbc_tea',
+		r'cryptopp/build/pp_cbc_tea',
 		r'botan/build/bot_cbc_tea'
 	],
 
 	# TEA OFB
 	[
 		r'test/build/test_ofb_tea_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_ofb_tea',
+		r'tomcrypt/build/tom_ofb_tea',
 		r'botan/build/bot_ofb_tea'
 	],
 
 	# TEA CFB
 	[
 		r'test/build/test_cfb_tea_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_cfb_tea',
+		r'tomcrypt/build/tom_cfb_tea',
 		r'botan/build/bot_cfb_tea'
 	],
 
 	# TEA CTR
 	[
 		r'test/build/test_ctr_tea_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_ctr_tea'
+		r'tomcrypt/build/tom_ctr_tea'
 	],
 
 	# MD5
 	[
 		r'test/build/test_md5_[gc]o[0-9sz]',
-		r'openssl/build/openssl_md5',
-		r'cryptopp/build/cryptopp_md5',
-		r'tomCrypt/build/tom_md5',
+		r'openssl/build/open_md5',
+		r'cryptopp/build/pp_md5',
+		r'tomcrypt/build/tom_md5',
 		r'nettle/build/net_md5'
 	],
 
 	# MD5 HMAC
 	[
 		r'test/build/test_hmac_md5_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_hmac_md5',
+		r'tomcrypt/build/tom_hmac_md5',
 		r'botan/build/bot_hmac_md5',
-		r'openssl/build/openssl_hmac_md5',
+		r'openssl/build/open_hmac_md5',
 		r'nettle/build/net_hmac_md5',
-		r'cryptopp/build/cryptopp_hmac_md5'
+		r'cryptopp/build/pp_hmac_md5'
 	],
 	
 	# RC4
 	[
 		r'test/build/test_rc4_[gc]o[0-9sz]',
-		r'openssl/build/openssl_rc4',
-		r'cryptopp/build/cryptopp_rc4',
-		r'tomCrypt/build/tom_rc4',
+		r'openssl/build/open_rc4',
+		r'cryptopp/build/pp_rc4',
+		r'tomcrypt/build/tom_rc4',
 		r'botan/build/bot_rc4',
 		r'nettle/build/net_rc4',
 	],
@@ -81,9 +81,9 @@ ckeck_list = [
 	# AES
 	[
 		r'test/build/test_aes_[gc]o[0-9sz]',
-		r'openssl/build/openssl_aes',
-		r'cryptopp/build/cryptopp_aes',
-		r'tomCrypt/build/tom_aes',
+		r'openssl/build/open_aes',
+		r'cryptopp/build/pp_aes',
+		r'tomcrypt/build/tom_aes',
 		r'botan/build/bot_aes',
 		r'nettle/build/net_aes'
 	],
@@ -91,56 +91,56 @@ ckeck_list = [
 	# AES ECB
 	[
 		r'test/build/test_ecb_aes_[gc]o[0-9sz]',
-		r'openssl/build/openssl_ecb_aes',
-		r'tomCrypt/build/tom_ecb_aes',
+		r'openssl/build/open_ecb_aes',
+		r'tomcrypt/build/tom_ecb_aes',
 		r'botan/build/bot_ecb_aes'
 	],
 
 	# AES CBC
 	[
 		r'test/build/test_cbc_aes_[gc]o[0-9sz]',
-		r'openssl/build/openssl_cbc_aes',
-		r'tomCrypt/build/tom_cbc_aes',
+		r'openssl/build/open_cbc_aes',
+		r'tomcrypt/build/tom_cbc_aes',
 		r'botan/build/bot_cbc_aes',
 		r'nettle/build/net_cbc_aes',
-		r'cryptopp/build/cryptopp_cbc_aes'
+		r'cryptopp/build/pp_cbc_aes'
 	],
 
 	# AES OFB
 	[
 		r'test/build/test_ofb_aes_[gc]o[0-9sz]',
-		r'openssl/build/openssl_ofb_aes',
-		r'tomCrypt/build/tom_ofb_aes',
+		r'openssl/build/open_ofb_aes',
+		r'tomcrypt/build/tom_ofb_aes',
 		r'botan/build/bot_ofb_aes'
 	],
 
 	# AES CFB
 	[
 		r'test/build/test_cfb_aes_[gc]o[0-9sz]',
-		r'openssl/build/openssl_cfb_aes',
-		r'tomCrypt/build/tom_cfb_aes',
+		r'openssl/build/open_cfb_aes',
+		r'tomcrypt/build/tom_cfb_aes',
 		r'botan/build/bot_cfb_aes'
 	],
 
 	# AES CTR (endianess 1)
 	[
 		r'test/build/test_ctr_aes_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_ctr_aes'
+		r'tomcrypt/build/tom_ctr_aes'
 	],
 
 	# AES CTR (endianess 2)
 	[
-		r'openssl/build/openssl_ctr_aes',
+		r'openssl/build/open_ctr_aes',
 		r'botan/build/bot_ctr_aes',
-		r'cryptopp/build/cryptopp_ctr_aes'
+		r'cryptopp/build/pp_ctr_aes'
 	],
 
 	# SHA1
 	[
 		r'test/build/test_sha1_[gc]o[0-9sz]',
-		r'openssl/build/openssl_sha1',
-		r'cryptopp/build/cryptopp_sha1',
-		r'tomCrypt/build/tom_sha1',
+		r'openssl/build/open_sha1',
+		r'cryptopp/build/pp_sha1',
+		r'tomcrypt/build/tom_sha1',
 		r'botan/build/bot_sha1',
 		r'nettle/build/net_sha1',
 	],
@@ -148,31 +148,31 @@ ckeck_list = [
 	# SHA1 HMAC
 	[
 		r'test/build/test_hmac_sha1_[gc]o[0-9sz]',
-		r'tomCrypt/build/tom_hmac_sha1',
+		r'tomcrypt/build/tom_hmac_sha1',
 		r'botan/build/bot_hmac_sha1'
 	],
 
 	# SERPENT
 	[
 		r'test/build/test_serpent_[gc]o[0-9sz]',
-		r'cryptopp/build/cryptopp_serpent',
+		r'cryptopp/build/pp_serpent',
 		r'botan/build/bot_serpent'
 	],
 	
 	# DES
 	[
 		r'test/build/test_des_[gc]o[0-9sz]',
-		r'openssl/build/openssl_des',
-		r'cryptopp/build/cryptopp_des',
-		r'tomCrypt/build/tom_des',
+		r'openssl/build/open_des',
+		r'cryptopp/build/pp_des',
+		r'tomcrypt/build/tom_des',
 		r'botan/build/bot_des'
 	],
 	
 	# TWOFISH
 	[
 		r'test/build/test_twofish_[gc]o[0-9sz]',
-		r'cryptopp/build/cryptopp_twofish',
-		r'tomCrypt/build/tom_twofish',
+		r'cryptopp/build/pp_twofish',
+		r'tomcrypt/build/tom_twofish',
 		r'botan/build/bot_twofish'
 	],
 
@@ -203,7 +203,7 @@ def execute_file(file_name):
 	process.wait()
 
 	if process.returncode != 0:
-		sys.stdout.write('\x1b[31mFAIL\x1b[0m\x1b[0m (return code: " + str(process.returncode) + ")\n')
+		sys.stdout.write('\x1b[31mFAIL\x1b[0m\x1b[0m (return code: ' + str(process.returncode) + ')\n')
 	return output[0]
 
 if __name__=='__main__':
